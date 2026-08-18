107320f7c5008bfd1c1bca88cf65db0ca6c34c21
ryneskabo <ryneskabo@outlook.com>
2026-08-18 00:07:29 -0500

    Added root index.html file, also added /assets/logo.png with chirpy

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import ("fmt";
 
 func main() {
 	serveMux := http.NewServeMux()
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
 	server := http.Server{
 		Handler: serveMux,
 		Addr: ":8080",
