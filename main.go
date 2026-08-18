a95fe85cc0254f6190c83b49605e6114d64d747a
ryneskabo <ryneskabo@outlook.com>
2026-08-18 18:35:16 -0500

    Added metrics endpoint at /metrics, which tracks number of hits to /app/, also added /reset endpoint to reset the metrics without restarting the server

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import ("fmt";
-	"net/http";
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func ReadinessHandler(writer http.ResponseWriter, req *http.Request) {
 	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -16,15 +29,39 @@ func ReadinessHandler(writer http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func (cfg *apiConfig) HitsHandler(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	hitsString := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	_, err := writer.Write([]byte(hitsString))
+	if err != nil {
+		err := fmt.Errorf("Error in hits handler write: %w", err)
+		fmt.Println(err)
+	}
+}
+
+func (cfg *apiConfig) ResetHitsHandler(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+}
+
 func main() { 
 	const port = "8080"
 
 	serveMux := http.NewServeMux()
 	strippedPathHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
-	serveMux.Handle("/app/", strippedPathHandler)
+	apiCfg := apiConfig{}
+	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(strippedPathHandler))
 
 	// Handles Readiness Endpoint, shows if server is up and running
 	serveMux.HandleFunc("/healthz", ReadinessHandler)
+
+	// Handles Hits Metrics Endpoint, shows how many hits since last reset
+	serveMux.HandleFunc("/metrics", apiCfg.HitsHandler)
+
+	// Handles reset Metrics Endpoint, resets the number of hits
+	serveMux.HandleFunc("/reset", apiCfg.ResetHitsHandler)
 
 	// Initializes server struct and starts it using the serveMux handler
 	server := http.Server{
