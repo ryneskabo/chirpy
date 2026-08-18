7079f7b573a3125d697c9f0db9da7d452875328f
ryneskabo <ryneskabo@outlook.com>
2026-08-18 02:10:16 -0500

    Added comments for future reference

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import ("fmt";
 func ReadinessHandler(writer http.ResponseWriter, req *http.Request) {
 	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	writer.WriteHeader(http.StatusOK)
+
+	// Writes out OK to body of response
 	_, err := writer.Write([]byte("OK"))
 	if err != nil {
 		err := fmt.Errorf("Error in readiness handler body write: %w", err)
@@ -15,13 +17,19 @@ func ReadinessHandler(writer http.ResponseWriter, req *http.Request) {
 }
 
 func main() { 
+	const port = "8080"
+
 	serveMux := http.NewServeMux()
 	strippedPathHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 	serveMux.Handle("/app/", strippedPathHandler)
+
+	// Handles Readiness Endpoint, shows if server is up and running
 	serveMux.HandleFunc("/healthz", ReadinessHandler)
+
+	// Initializes server struct and starts it using the serveMux handler
 	server := http.Server{
 		Handler: serveMux,
-		Addr: ":8080",
+		Addr: ":" + port,
 	}
 	err := server.ListenAndServe()
 	if err != nil {
